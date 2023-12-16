f6c88a5b752f61210dbe6de72636150ccf048fe5
Oduigwe Gerald <paulsongerald00@gmail.com>
2023-12-16 21:56:00 +0100

    revising DSA problems

--- rough.js
+++ rough.js
@@ -14,3 +14,26 @@ console.log((5).toString(2));
 console.log((4).toString(2));
 console.log((10).toString(2));
 console.log((9).toString(2));
+
+// fib
+function fib(n) {
+  let seq = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    seq[i] = seq[i - 2] + seq[i - 1];
+  }
+  return seq;
+}
+
+console.log(fib(10));
+
+// fac
+function fac(n) {
+  num = 1;
+  for (i = n; i > 0; i--) {
+    num *= i;
+  }
+  return num;
+}
+
+console.log(fac(4));
+console.log(fac(5));
